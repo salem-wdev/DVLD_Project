753e6659afb4fa4fd847df0276389ec81b5c77eb
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-05-19 13:36:21 +0300

    feat(UI): enable support for editing existing appointments

--- DVLD/Tests/frmScheduleTest.cs
+++ DVLD/Tests/frmScheduleTest.cs
@@ -14,23 +14,37 @@ namespace DVLD.Tests
     public partial class frmScheduleTest : Form
     {
         private int _LocalDrivingLicenseApplicationID = -1;
+        private int _TestAppointmentID = -1;
         public int LocalDrivingLicenseApplicationID
         {
             get => _LocalDrivingLicenseApplicationID;
         }
+        public int TestAppointmentID
+        {
+            get => _TestAppointmentID;
+        }
         private clsTestType.enTestType _TestType;
 
-        public frmScheduleTest(int LocalDrivingLicenseApplicationID, clsTestType.enTestType TestType)
+        public frmScheduleTest(int LocalDrivingLicenseApplicationID, clsTestType.enTestType TestType,int TestAppointmentID = -1)
         {
             InitializeComponent();
             _LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplicationID;
+            _TestAppointmentID = TestAppointmentID;
             _TestType = TestType;
         }
 
 
         private void frmScheduleTest_Load(object sender, EventArgs e)
         {
-            ctrlScheduleTest1.LoadData(_LocalDrivingLicenseApplicationID, _TestType);
+            if (_TestAppointmentID == -1)
+            {
+                ctrlScheduleTest1.LoadData(_LocalDrivingLicenseApplicationID, _TestType);
+            }
+            else
+            {
+                ctrlScheduleTest1.LoadData(_LocalDrivingLicenseApplicationID, _TestType, _TestAppointmentID);
+
+            }
         }
     }
 }
