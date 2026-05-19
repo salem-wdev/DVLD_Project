2ae9192207b8c4d18bbc60f4a86fa068a5d050fe
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-05-19 10:11:45 +0300

    feat(UI): Add form frmScheduleTest

---
+++ DVLD/Tests/frmScheduleTest.cs
@@ -0,0 +1,36 @@
+﻿using DVLD_Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Tests
+{
+    public partial class frmScheduleTest : Form
+    {
+        private int _LocalDrivingLicenseApplicationID = -1;
+        public int LocalDrivingLicenseApplicationID
+        {
+            get => _LocalDrivingLicenseApplicationID;
+        }
+        private clsTestType.enTestType _TestType;
+
+        public frmScheduleTest(int LocalDrivingLicenseApplicationID, clsTestType.enTestType TestType)
+        {
+            InitializeComponent();
+            _LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplicationID;
+            _TestType = TestType;
+        }
+
+
+        private void frmScheduleTest_Load(object sender, EventArgs e)
+        {
+            ctrlScheduleTest1.LoadData(_LocalDrivingLicenseApplicationID, _TestType);
+        }
+    }
+}
