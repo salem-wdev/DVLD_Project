8d22a7287caf64ef4da7859f2328297b7e34fb2e
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-05-25 10:12:01 +0300

    feat(UI): Add control to show license info

---
+++ DVLD/Licenses/Controls/ctrlDriverLicenseInfo.cs
@@ -0,0 +1,119 @@
+﻿using DVLD.Properties;
+using DVLD_Business;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLD.Classes;
+
+namespace DVLD.Licenses.Controls
+{
+    public partial class ctrlDriverLicenseInfo : UserControl
+    {
+        private int _LicenseID;
+        private clsLicense _License;
+
+        public ctrlDriverLicenseInfo()
+        {
+            InitializeComponent();
+
+        }
+
+        public int LicenseID
+        {
+            get { return _LicenseID; }
+        }
+
+        public clsLicense SelectedLicenseInfo
+        { get { return _License; } }
+
+        private void _LoadPersonImage()
+        {
+            if (_License.DriverInfo.PersonInfo.Gender == 0)
+                pbPersonImage.Image = Resources.Male_512;
+            else
+                pbPersonImage.Image = Resources.Female_512;
+
+            string ImagePath = _License.DriverInfo.PersonInfo.ImagePath;
+
+            if (ImagePath != "")
+                if (File.Exists(ImagePath))
+                    pbPersonImage.Load(ImagePath);
+                else
+                    MessageBox.Show("Could not find this image: = " + ImagePath, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void _FillControlWithData()
+        {
+            lblLicenseID.Text = _License.LicenseID.ToString();
+            lblIsActive.Text = _License.IsActive ? "Yes" : "No";
+            lblIsDetained.Text = _License.IsDetained ? "Yes" : "No";
+            lblClass.Text = _License.LicenseClassIfo.ClassName;
+            lblFullName.Text = _License.DriverInfo.PersonInfo.FullName;
+            lblNationalNo.Text = _License.DriverInfo.PersonInfo.NationalNo;
+            lblGendor.Text = _License.DriverInfo.PersonInfo.Gender == 0 ? "Male" : "Female";
+            lblDateOfBirth.Text = clsFormat.DateToShort(_License.DriverInfo.PersonInfo.DateOfBirth);
+
+            lblDriverID.Text = _License.DriverID.ToString();
+            lblIssueDate.Text = clsFormat.DateToShort(_License.IssueDate);
+            lblExpirationDate.Text = clsFormat.DateToShort(_License.ExpirationDate);
+            lblIssueReason.Text = _License.IssueReasonText;
+            lblNotes.Text = _License.Notes == "" ? "No Notes" : _License.Notes;
+            _LoadPersonImage();
+        }
+
+        private void _ResetControl()
+        {
+            _LicenseID = -1;
+            _License = null;
+
+            lblLicenseID.Text = "[????]";
+            lblIsActive.Text = "[????]";
+            lblIsDetained.Text = "[????]";
+            lblClass.Text = "[????]";
+            lblFullName.Text = "[????]";
+            lblNationalNo.Text = "[????]";
+            lblGendor.Text = "[????]";
+            lblDateOfBirth.Text = "[????]";
+
+            lblDriverID.Text = "[????]";
+            lblIssueDate.Text = "[????]";
+            lblExpirationDate.Text = "[????]";
+            lblIssueReason.Text = "[????]";
+            lblNotes.Text = "[????]";
+
+            pbPersonImage.Image = Resources.Male_512;
+        }
+
+        public void ClearData()
+        {
+            _ResetControl();
+        }
+
+        public bool LoadData(int LicenseID)
+        {
+            _LicenseID = LicenseID;
+            _License = clsLicense.Find(_LicenseID);
+            if (_License == null)
+            {
+                MessageBox.Show("Could not find License ID = " + _LicenseID.ToString(),
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                _LicenseID = -1;
+                return false;
+            }
+            else
+            {
+                _FillControlWithData();
+                return true;
+            }
+
+        }
+    }
+}
