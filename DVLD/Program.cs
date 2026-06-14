ef67dc51a7cf801d27f45d9beccda675476fc454
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-06-14 09:15:53 +0300

    feat(program): process system expirations on startup

--- DVLD/Program.cs
+++ DVLD/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DVLD_Business;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,24 @@ namespace DVLD
         [STAThread]
         static void Main()
         {
+            if (!clsLicense.DeactivateExpiredLicenses())
+            {
+                MessageBox.Show("An error occurred while deactivating expired licenses. Please contact support.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!clsInternationalLicense.DeactvateExpiredLicenses())
+            {
+                MessageBox.Show("An error occurred while deactivating expired international licenses. Please contact support.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!clsTestAppointment.LockExpiredTestAppointments())
+            {
+                MessageBox.Show("An error occurred while locking expired test appointments. Please contact support.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new frmMain());
