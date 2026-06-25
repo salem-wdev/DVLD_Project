0d995406eaaa2e82b071c2d4dcdebf8aecf9bd4a
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-06-25 13:35:52 +0300

    Refactor clsGlobal and clean up unused resources
    Moved clsGlobal.cs to DVLD_Business and refactored it to include methods for managing user credentials and retrieving server time. Updated DVLD_Business.csproj to reflect the new structure. Removed unused files (test.cs, test.Designer.cs, test.resx) and redundant resources from DVLD.csproj. Cleaned up project files for better modularity and maintainability.

---
+++ DVLD_Business/Global_Classes/clsGlobal.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using DVLD_Business;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_Business.Global_Classes
+{
+    internal static class clsGlobal
+    {
+        public static clsUser CurrentUser;
+
+        private static readonly string _filePath 
+            = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+                , "DVLD_Project\\data.txt");
+
+        public static bool RememberUsernameAndPassword(string Username, string Password)
+        {
+            try
+            {
+                string folderPath = Path.GetDirectoryName(_filePath);
+
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                if (Username == "" && File.Exists(_filePath))
+                {
+                    File.Delete(_filePath);
+                    return false;
+                }
+
+                string DataToSave = Username + "#//#" + Password;
+
+                using (StreamWriter writer = new StreamWriter(_filePath))
+                {
+                    writer.WriteLine(DataToSave);
+                    return true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while saving credentials: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return false;
+        }
+
+        public static bool GetStoredCredential(ref string Username, ref string Password)
+        {
+            try
+            {
+
+
+
+                if (File.Exists(_filePath))
+                {
+                    using (StreamReader reader = new StreamReader(_filePath))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            Console.WriteLine(line);
+                            string[] strings = line.Split(new string[] { "#//#" }, StringSplitOptions.None);
+                            Username = strings[0];
+                            Password = strings[1];
+                        }
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return false;
+
+        }
+
+        public static DateTime GetServerDateTime()
+        {
+            return clsBusinessSettings.GetServerDateTime();
+        }
+    }
+}
