8381f9dc45dec13122896424ff20963acc095213
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-06-27 19:57:18 +0300

    Update default database name in clsDataAccessSettings
    The `DataBaseName` property in the `clsDataAccessSettings` class was changed from the default value `"DVLD_Test"` to `"DVLD"`. This update also impacts the `ConnectionString` property, as it dynamically references the `DataBaseName` property.

--- DVLD_DataAccess/clsDataAccessSettings.cs
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -2,7 +2,7 @@
 {
     public class clsDataAccessSettings
     {
-        public static string DataBaseName { get; } = "DVLD_Test";
+        public static string DataBaseName { get; } = "DVLD";
         public static string ConnectionString { get; } = $"Server=.;Database={DataBaseName};User=sa;Password=123456";
     }
 }
