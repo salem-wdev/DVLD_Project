08ab34cf19947e7c3fd97f1623177e1c47a7f605
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-06-27 10:39:42 +0300

    Refactor clsDataAccessSettings for dynamic DB config
    Removed unused `using` directives to clean up the code. Updated the `DataBaseName` property to use `"DVLD_Test"` instead of `"DVLD"`. Refactored the `ConnectionString` property to dynamically reference the `DataBaseName` value using string interpolation. Reformatted the `namespace` declaration for better readability.

--- DVLD_DataAccess/clsDataAccessSettings.cs
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DVLD_DataAccess
+﻿namespace DVLD_DataAccess
 {
     public class clsDataAccessSettings
     {
-        public static string ConnectionString { get; } = "Server=.;Database=DVLD;User=sa;Password=123456";
-        public static string DataBaseName { get; } = "DVLD";
+        public static string DataBaseName { get; } = "DVLD_Test";
+        public static string ConnectionString { get; } = $"Server=.;Database={DataBaseName};User=sa;Password=123456";
     }
 }
