822de790f3458de9022789d3d3b8c68b95e40712
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-07-04 12:05:22 +0300

    Add support for international license applications
    Introduced a new form `frmNewInternationalLicenseApplication` to handle the creation and management of international license applications.
    
    - Added `frmNewInternationalLicenseApplication.Designer.cs`, `frmNewInternationalLicenseApplication.cs`, and `frmNewInternationalLicenseApplication.resx` files.
    - Implemented event handlers for form initialization, license selection, license issuance, and navigation to license history/details.
    - Integrated the new form into `frmMain.cs` by replacing the placeholder for the "International License" menu item.
    - Updated `DVLD.csproj` to include the new form and resource files in the build process.
    - Enhanced user experience by providing a complete workflow for issuing international licenses.

---
+++ DVLD/Applications/International License/frmNewInternationalLicenseApplication.cs
@@ -0,0 +1,96 @@
+﻿using DVLD.Licenses.International_Licenses;
+using DVLD.Licenses.Local_Licenses;
+using DVLD_Business;
+using DVLD_Business.Global_Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Applications.International_License
+{
+    public partial class frmNewInternationalLicenseApplication : Form
+    {
+        private int _NewInternationalLicenseID = -1;
+        private clsInternationalLicense _NewInternationalLicense = null;
+
+        public frmNewInternationalLicenseApplication()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNewInternationalLicenseApplication_Load(object sender, EventArgs e)
+        {
+            ctrlDriverLicenseInfoWithFilter1.OnLicenseSelected += CtrlDriverLicenseInfoWithFilter1_OnLicenseSelected;
+
+            AcceptButton = ctrlDriverLicenseInfoWithFilter1.AcceptButton;
+
+            lblApplicationDate.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            lblIssueDate.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            lblCreatedByUser.Text = clsGlobal.CurrentUser.UserName;
+            lblFees.Text = clsApplicationType.Find
+                ((int)clsApplication.enApplicationType.NewInternationalLicense)
+                .ApplicationTypeFees.ToString("0.##");
+        }
+
+        private void CtrlDriverLicenseInfoWithFilter1_OnLicenseSelected(int obj)
+        {
+            if (obj > 0)
+            {
+                //if (clsInternationalLicense.GetActiveInternationalLicenseIDByDriverID(ctrlDriverLicenseInfoWithFilter1.SelectedLicenseInfo.DriverID) > 0)
+                //{
+                //    MessageBox.Show("This Driver already has an Active International License.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //    return;
+                //}
+                _NewInternationalLicenseID = -1;
+                _NewInternationalLicense = null;
+                llShowLicenseHistory.Enabled = true;
+                btnIssueLicense.Enabled = true;
+                lblLocalLicenseID.Text = obj.ToString();
+            }
+        }
+
+        private void frmNewInternationalLicenseApplication_Activated(object sender, EventArgs e)
+        {
+            ctrlDriverLicenseInfoWithFilter1.txtLicenseIDFocus();
+        }
+
+        private void btnIssueLicense_Click(object sender, EventArgs e)
+        {
+            _NewInternationalLicense = clsInternationalLicense.IssueNewInternationalLicense(ctrlDriverLicenseInfoWithFilter1.SelectedLicenseInfo.DriverID, clsGlobal.CurrentUser.UserID);
+            if (_NewInternationalLicense == null)
+            {
+                llShowLicenseInfo.Enabled = false;
+                MessageBox.Show("Failed to Issue New International License.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            lblApplicationID.Text = _NewInternationalLicense.ApplicationID.ToString();
+            _NewInternationalLicenseID = _NewInternationalLicense.InternationalLicenseID;
+            lblInternationalLicenseID.Text = _NewInternationalLicenseID.ToString();
+            lblExpirationDate.Text = _NewInternationalLicense.ExpirationDate.ToString("dd/MM/yyyy");
+            MessageBox.Show("International License Issued Successfully with ID=" + _NewInternationalLicenseID.ToString(), "International License Issued", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            btnIssueLicense.Enabled = false;
+            ctrlDriverLicenseInfoWithFilter1.FilterEnabled = false;
+            llShowLicenseInfo.Enabled = true;
+        }
+
+        private void llShowLicenseHistory_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmShowPersonLicenseHistory frm = new frmShowPersonLicenseHistory(ctrlDriverLicenseInfoWithFilter1.SelectedLicenseInfo.DriverInfo.PersonID);
+            frm.ShowDialog();
+        }
+
+        private void llShowLicenseInfo_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmShowInternationalLicenseInfo frm = new frmShowInternationalLicenseInfo(_NewInternationalLicenseID);
+            frm.ShowDialog();
+        }
+    }
+}
