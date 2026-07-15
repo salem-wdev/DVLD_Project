e18145b40c2c1c86536797677c09fa1703044078
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-07-15 19:08:09 +0300

    Refactor license selection event handling
    Replaced the old `OnLicenseSelected` event handler with a new strongly-typed `LicenseSelected` event handler in `frmReplaceLostOrDamagedLicenseApplication`. Updated the designer file to connect the new event handler. Removed the obsolete `CtrlDriverLicenseInfoWithFilter1_OnLicenseSelected` method and its subscription. The new handler improves type safety and maintains the same functionality for license validation and UI updates.

--- DVLD/Applications/ReplaceLostOrDamagedLicense/frmReplaceLostOrDamagedLicenseApplication.Designer.cs
+++ DVLD/Applications/ReplaceLostOrDamagedLicense/frmReplaceLostOrDamagedLicenseApplication.Designer.cs
@@ -397,6 +397,7 @@
             this.ctrlDriverLicenseInfoWithFilter1.Name = "ctrlDriverLicenseInfoWithFilter1";
             this.ctrlDriverLicenseInfoWithFilter1.Size = new System.Drawing.Size(874, 413);
             this.ctrlDriverLicenseInfoWithFilter1.TabIndex = 199;
+            this.ctrlDriverLicenseInfoWithFilter1.LicenseSelected += new System.EventHandler<DVLD.Licenses.Local_Licenses.Controls.ctrlDriverLicenseInfoWithFilter.LicenseSelectedEventArgs>(this.ctrlDriverLicenseInfoWithFilter1_LicenseSelected);
             // 
             // frmReplaceLostOrDamagedLicenseApplication
             // 
