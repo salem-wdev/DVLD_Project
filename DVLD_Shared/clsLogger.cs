9e77b5c0fb852fe6bc4c80329a3a05183dc421c0
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-07-28 15:48:42 +0300

    add debug logging utility
    The clsLogger class has been converted to a static class to provide utility methods. A new LogLevel enum has been introduced to define logging levels (Info, Warning, Error).
    
    A conditional Log method has been added, which formats log messages with a timestamp and log level, and writes them to the debug output. This method is only included in DEBUG builds.

--- DVLD_Shared/clsLogger.cs
+++ DVLD_Shared/clsLogger.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DVLD_Shared
 {
-    public class clsLogger
+    public static class clsLogger
     {
+        public enum LogLevel { Info, Warning, Error }
+
+        [Conditional("DEBUG")]
+        public static void Log(string message, LogLevel level = LogLevel.Info)
+        {
+            string formattedMessage = $"[{level.ToString().ToUpper()}] {DateTime.Now}: {message}";
+            System.Diagnostics.Debug.WriteLine(formattedMessage);
+        }
     }
 }
