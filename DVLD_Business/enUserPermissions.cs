9271c251bfbd606e8bb8a74278e37fa6a955873f
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-07-29 12:10:53 +0300

    Add enUserPermissions enum for user permission management
    A new file, `enUserPermissions.cs`, was added to the project, and its inclusion was reflected in the `DVLD_Business.csproj` file.
    
    The file introduces the `enUserPermissions` enumeration within the `DVLD_Business` namespace. This enum, decorated with the `[Flags]` attribute, defines various user permission levels (`None`, `ManageUsers`, `ManageDrivers`, `ManageLicenses`, `ManageApplications`, and `All`) to support bitwise operations for permission management.

---
+++ DVLD_Business/enUserPermissions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DVLD_Business
+{
+    [Flags]
+    public enum enUserPermissions : int
+    {
+        None = 0,
+        ManageUsers = 1,
+        ManageDrivers = 2,
+        ManageLicenses = 4,
+        ManageApplications = 8,
+        All = -1
+    }
+}
