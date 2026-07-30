ac992766feb6d6faa7f26b8ce78d7d3304fc38a1
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-07-30 13:47:13 +0300

    Add EnforcePermissionAttribute for method-level security
    A new file, `EnforcePermissionAttribute.cs`, was added to the project. This file introduces the `EnforcePermissionAttribute` class, which is an attribute designed to enforce user permissions at the method level.
    
    The attribute requires a specific `enUserPermissions` enum value to be passed to its constructor, which is stored in the `Permission` property. It is restricted to be applied only to methods, as specified by the `[AttributeUsage]` declaration.
    
    The project file `DVLD_Business.csproj` was updated to include the new `EnforcePermissionAttribute.cs` file.

---
+++ DVLD_Business/Users/EnforcePermissionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business.Users
+{
+    // Restriction: This attribute can only be applied to methods, not classes or properties.
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class EnforcePermissionAttribute : Attribute
+    {
+        // Property to hold the required permission for the decorated method
+        public enUserPermissions Permission { get; }
+
+        // Constructor that accepts the specific enum value as the security "lock"
+        public EnforcePermissionAttribute(enUserPermissions permission)
+        {
+            Permission = permission;
+        }
+    }
+}
+
