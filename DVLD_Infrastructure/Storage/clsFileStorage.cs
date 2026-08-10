cfd778d73885bfc44b4157a8a7aae58929181e0d
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-08-10 12:03:48 +0300

    Refactor: move clsFileStorage to DVLD_Infrastructure project
    The `clsFileStorage.cs` file was moved from the `DVLD_Shared`
    project to the `DVLD_Infrastructure` project. Its namespace
    was updated from `DVLD_Business.Global_Classes` to
    `DVLD_Infrastructure.Storage` to reflect this change.
    The `DVLD_Shared.csproj` file was updated to remove the
    reference to `clsFileStorage.cs`, and the `DVLD_Infrastructure.csproj`
    file was updated to include it.

--- DVLD_Infrastructure/Storage/clsFileStorage.cs
+++ DVLD_Infrastructure/Storage/clsFileStorage.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DVLD_Business.Global_Classes
+namespace DVLD_Infrastructure.Storage
 {
     public class clsFileStorage
     {
