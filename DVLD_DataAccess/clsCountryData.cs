92b8c7cca9d8f9c9a4758d7bbc6ecf08a7dee83c
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-08-16 11:40:23 +0300

    Refactor clsCountryData to use async methods
    Replaced synchronous methods with asynchronous equivalents to improve scalability and responsiveness. Introduced `_connectionString` for better maintainability. Updated `GetCountryByID`, `GetCountryByCountryName`, and `GetAllCountries` to use `async/await` and refined SQL queries for efficiency. Enhanced exception handling with detailed logging via `clsLogger`. Replaced `AddWithValue` with strongly-typed SQL parameters for better performance. Added `ConfigureAwait(false)` to all `await` calls to prevent potential deadlocks.

--- DVLD_DataAccess/clsCountryData.cs
+++ DVLD_DataAccess/clsCountryData.cs
@@ -2,100 +2,102 @@ using System;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Threading.Tasks;
 using DVLD_Infrastructure.Storage;
 
 namespace DVLD_DataAccess
 {
     public class clsCountryData
     {
+        private static readonly string _connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
 
-        public static bool GetCountryByID(int CountryID, ref string CountryName)
+        public static async Task<string> GetCountryByIDAsync(int CountryID)
         {
-            bool IsFound = false;
+            string CountryName = null;
             try
             {
-                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+                using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
-                    string Query = "SELECT * FROM Countries WHERE CountryID = @CountryID";
+                    string Query = "SELECT CountryName FROM Countries WHERE CountryID = @CountryID";
+
                     using (SqlCommand Command = new SqlCommand(Query, connection))
                     {
-                        Command.Parameters.AddWithValue("@CountryID", CountryID);
+                        Command.Parameters.Add("@CountryID", SqlDbType.Int).Value = CountryID;
 
-                        connection.Open();
-                        using (SqlDataReader reader = Command.ExecuteReader())
+                        await connection.OpenAsync().ConfigureAwait(false);
+                        using (SqlDataReader reader = await Command.ExecuteReaderAsync(CommandBehavior.SingleRow).ConfigureAwait(false))
                         {
-                            if (reader.Read())
+                            if (await reader.ReadAsync().ConfigureAwait(false))
                             {
-                                CountryName = reader["CountryName"].ToString();
-                                IsFound = true;
-                            }  
-                        } 
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-            }
-            return IsFound;
-        }
-
-        public static bool GetCountryByCountryName(string CountryName, ref int CountryID)
-        {
-            bool IsFound = false;
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
-                {
-                    string Query = "SELECT * FROM Countries WHERE CountryName = @CountryName";
-                    using (SqlCommand Command = new SqlCommand(Query, connection))
-                    {
-                        Command.Parameters.AddWithValue("@CountryName", CountryName);
-
-                        connection.Open();
-                        using (SqlDataReader reader = Command.ExecuteReader())
-                        {
-                            if (reader.Read())
-                            {
-                                CountryID = (int)reader["CountryID"];
-                                IsFound = true;
-                            }   
+                                CountryName = reader.GetString(0);
+                            }
                         }
                     }
                 }
             }
             catch (Exception ex)
             {
+                clsLogger.LogException(ex, $"Error occurred in GetCountryByIDAsync method for CountryID {CountryID}");
             }
-            return IsFound;
+            return CountryName;
         }
 
-
-        public static DataTable GetAllCountries()
+        public static async Task<int?> GetCountryByCountryNameAsync(string CountryName)
         {
-            DataTable Table = new DataTable();
+            int? CountryID = null;
             try
             {
-                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+                using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
-                    string Query = "SELECT * FROM Countries";
+                    string Query = "SELECT CountryID FROM Countries WHERE CountryName = @CountryName";
+                    using (SqlCommand Command = new SqlCommand(Query, connection))
+                    {
+                        Command.Parameters.Add("@CountryName", SqlDbType.NVarChar, 50).Value = CountryName;
+
+                        await connection.OpenAsync().ConfigureAwait(false);
+                        using (SqlDataReader reader = await Command.ExecuteReaderAsync(CommandBehavior.SingleRow).ConfigureAwait(false))
+                        {
+                            if (await reader.ReadAsync().ConfigureAwait(false))
+                                CountryID = reader.GetInt32(0);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                clsLogger.LogException(ex, $"Error occurred in GetCountryByCountryName method for CountryName: {CountryName}");
+            }
+            return CountryID;
+        }
+
+        public static async Task<DataTable> GetAllCountriesAsync()
+        {
+            DataTable Table = new DataTable();
+            Table.Columns.Add("CountryID", typeof(int));
+            Table.Columns.Add("CountryName", typeof(string));
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    string Query = "SELECT CountryID, CountryName FROM Countries";
 
                     using (SqlCommand Command = new SqlCommand(Query, connection))
                     {
 
-                        connection.Open();
-                        using (SqlDataReader reader = Command.ExecuteReader())
+                        await connection.OpenAsync().ConfigureAwait(false);
+                        using (SqlDataReader reader = await Command.ExecuteReaderAsync().ConfigureAwait(false))
                         {
-                            Table.Load(reader);  
-                        } 
+                            while (await reader.ReadAsync().ConfigureAwait(false))
+                                Table.Rows.Add(reader.GetInt32(0), reader.GetString(1));
+                        }
                     }
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                clsLogger.LogException(ex, "Error occurred in GetAllCountries method");
             }
             return Table;
         }
-
-
     }
 }
